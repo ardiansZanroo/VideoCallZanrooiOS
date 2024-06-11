f253a1a103c69405e68b32777868cae00a28dd72
ardianZanroo <ardiansyah@zanroo.com>
2024-06-11 14:39:34 +0700

    add privacy
    add privacy

--- Package.swift
+++ Package.swift
@@ -6,21 +6,33 @@ import PackageDescription
 let package = Package(
     name: "ZanrooCallWebPackage",
     platforms: [
-           .iOS(.v12), // Adjust the minimum version as needed
-       ],
+        .iOS(
+            .v12
+        ),
+        // Adjust the minimum version as needed
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ZanrooCallWebPackage",
-            targets: ["ZanrooCallWebPackage"]),
+            targets: ["ZanrooCallWebPackage"]
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ZanrooCallWebPackage"),
-        .testTarget(
-            name: "ZanrooCallWebPackageTests",
-            dependencies: ["ZanrooCallWebPackage"]),
+            name: "ZanrooCallWebPackage",
+            resources: [
+                .process(
+                    "Resources/PrivacyInfo.xcprivacy"
+                )
+            ]
+        ),
+        
+            .testTarget(
+                name: "ZanrooCallWebPackageTests",
+                dependencies: ["ZanrooCallWebPackage"]
+            ),
     ]
 )
